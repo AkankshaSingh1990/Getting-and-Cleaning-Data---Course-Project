64863c6e515c8a11e3002d1812b4f657adf92826
AkankshaSingh1990 <Akanksha.p20@gmail.com>
2014-07-26 21:41:35 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -19,7 +19,7 @@
 # 1. Merge the training and the test sets to create one data set.
 
 #set working directory to the location where the UCI HAR Dataset was unzipped
-setwd('/Users/heather/Documents/UCI HAR Dataset/');
+setwd('C:/Users/dell/Documents/UCI HAR Dataset/');
 
 # Read in the data from files
 features     = read.table('./features.txt',header=FALSE); #imports features.txt
